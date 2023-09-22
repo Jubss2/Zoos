7dabe38ad2ef1f1340524c33c7ddcc6d25c406d1
Jubss2 <ju.araujo.0300@gmail.com>
2023-09-21 23:14:12 -0300

    Salas

--- Assets/Scripts/SwordAttack.cs
+++ Assets/Scripts/SwordAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SwordAttack : MonoBehaviour
 {
     private float time;
+    public float damage;
     private void Update()
     {
         time += Time.deltaTime;
@@ -20,7 +21,7 @@ public class SwordAttack : MonoBehaviour
     {
         if (collision.tag == "Inimigo")
         {
-            collision.GetComponent<Enemy1Controller>().DealDamage(1);
+            collision.GetComponent<Enemy1Controller>().DealDamage(damage);
         }
     }
 }
