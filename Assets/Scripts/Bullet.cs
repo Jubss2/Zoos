01fdc351525fdd59cef0a9dd038d351da3c709fc
Jubss2 <ju.araujo.0300@gmail.com>
2023-11-11 12:09:33 -0300

    Ajeitei bala

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,12 +8,22 @@ public class Bullet : MonoBehaviour
     private Vector3 movimento;
     private Rigidbody2D rb;
     private float time = 0f;
+
+    public void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 0.5f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
     public void SetMovimento(Vector3 movimento)
     {
         rb = GetComponent<Rigidbody2D>();
         this.movimento = movimento;
         rb.velocity = movimento * speed;
-        DestroyBullet();
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -27,15 +37,4 @@ public class Bullet : MonoBehaviour
             collision.GetComponent<Quebravel>().DealDamage(1);
         }
     }
-
-    private void DestroyBullet()
-    {
-            time += Time.deltaTime;
-            if (time > 0.6f)
-            {
-                Destroy(gameObject);
-            }
-        
-    }
-    
 }
